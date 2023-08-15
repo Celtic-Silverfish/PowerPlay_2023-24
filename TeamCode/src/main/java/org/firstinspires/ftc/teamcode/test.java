77ce17b386529dfa6b3d830c02e0af53b18431a3
Emmykay07 <emmykay2111@gmail.com>
2023-08-14 21:20:23 -0700

    Auto Instruction for motor spinny
    Autonomous program setup instructions for motor spinning forward and backwards

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -0,0 +1,34 @@
+/*
+motor goes forward for 5 seconds
+stops
+motor goes backwards for 5 seconds
+
+what needs to be made
+Initialization
+1. Declare and initialize variables (doubles + DCMotor)
+2. Hardwaremap
+3. Set Direction
+4. Update Telemetry
+Run
+1. Set Power using final double variable
+2. While loop with time constraint
+3. Turn off power
+4. Set Power and reverse
+5. Update Telemetry
+ */
+
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous (name = "robot owo")
+public class test extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
