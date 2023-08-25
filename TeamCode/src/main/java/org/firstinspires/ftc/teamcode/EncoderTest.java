625ca4a19c250681314c509adf6bebed249dfaeb
Emmykay07 <emmykay2111@gmail.com>
2023-08-24 21:24:18 -0700

    Attempted Encoder program
    Tried to copy off the FTC sample code in the SDK, so far encoderDrive variable is being difficult...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -10,36 +10,46 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class EncoderTest extends LinearOpMode {
 
     DcMotor leftDrive = null;
+    DcMotor rightDrive = null;
     ElapsedTime runtime;
 
     {
-        runtime = new ElapsedTime;
+        runtime = new ElapsedTime();
     }
 
-    final double Speed = 0.75;
+    final double DRIVE_SPEED = 0.75;
+    final double TURN_SPEED = 0.5;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
         leftDrive = hardwareMap.get(DcMotor.class, "LD");
+        rightDrive = hardwareMap.get(DcMotor.class, "RD");
 
         leftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        double position = leftDrive.getCurrentPosition();
-        telemetry.addData("Encoder Position", position);
+        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        telemetry.addData("Starting position", "%7d: %7d",
+                leftDrive.getCurrentPosition(),
+                rightDrive.getCurrentPosition());
         telemetry.update();
 
         waitForStart();
 
-        leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        leftDrive.setTargetPosition(1120);
-        leftDrive.setPower(Speed);
-        telemetry.update();
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+
 
         leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        public void encoderDrive(double speed, double leftinches, double rightinches, double timeoutS) {
 
     }
 }
